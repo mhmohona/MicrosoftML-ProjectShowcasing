fa0339b69d3c69bb4c77bc2759e12378e642b4be
Quy Vu <xquy.vu@gmail.com>
2020-08-29 16:54:31 +0100

    create database and split data into train and test

---
+++ project/Quy Vu/src/database_setup/create_schema.sql
@@ -0,0 +1,15 @@
+create table patients (
+	"age" int,
+    "anaemia" int,
+    "creatinine_phosphokinase" double precision,
+    "diabetes" int,
+    "ejection_fraction" double precision,
+    "high_blood_pressure" int,
+    "platelets" double precision,
+    "serum_creatinine" double precision,
+    "serum_sodium" double precision,
+    "sex" int,
+    "smoking" int,
+    "time" int,
+    "DEATH_EVENT" int
+)
